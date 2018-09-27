91d286318d210f52228633ccf1a8ac6efdba0afe
Aweba13 <fiorella.effio@unmsm.edu.pe>
2018-09-27 18:54:11 -0500

    añadiendo media querie para home

--- src/js/animationHome.js
+++ src/js/animationHome.js
@@ -1,5 +1,10 @@
 var duration = anime({
-    targets: '#duration .el',
-    translateX: 200,
-    duration: 3000
-    });
+  targets: '#duration .el',
+  translateX: 100,
+  duration: 3500
+});
+var reverse = anime({
+  targets: '#reverse .myel',
+  translateX: -90,
+  duration: 3500
+});
